e1f37c440120e1f54e49cd54adf3b272e4e2639d
hussnain-uet-87 <hussnainuet87@gmail.com>
2025-12-24 17:48:42 +0500

    second commit

--- api/index.js
+++ api/index.js
@@ -1,36 +1,22 @@
 import express from "express";
-import mongoose from "mongoose";
-import dotenv from "dotenv";
-import path from "path";
-import { fileURLToPath } from "url";
 import cors from "cors";
+import dotenv from "dotenv";
 import connectDB from "../config.js";
 import salesRoutes from "../routes/sale.js";
 import analyticsRoute from "../routes/analyticsRoute.js";
 import loanRoutes from "../routes/loanRoutes.js";
 
-// Ensure we load the project's .env (located in server/) even when cwd is server/api
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-dotenv.config({ path: path.resolve(__dirname, "..", ".env") });
+dotenv.config();
+
 const app = express();
-app.use(cors({
-  origin: "https://zee-server.vercel.app" ,
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  credentials: true
-}));
-app.use(express.json())
 
+app.use(cors());
+app.use(express.json());
 
-// Routes
+connectDB();
+
 app.use("/api/sales", salesRoutes);
 app.use("/api/analytics", analyticsRoute);
 app.use("/api/loans", loanRoutes);
 
-
-// connect to MongoDB
-connectDB()
-
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
+export default app; // 🚨 THIS IS WHAT VERCEL NEEDS
