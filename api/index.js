96211b3db64e360e12fdb17f71187d083fea8f05
hussnain-uet-87 <hussnainuet87@gmail.com>
2025-12-24 23:13:24 +0500

    Resolved MongoDB error

--- api/index.js
+++ api/index.js
@@ -6,7 +6,13 @@ import salesRoutes from "../routes/sale.js";
 import analyticsRoute from "../routes/analyticsRoute.js";
 import loanRoutes from "../routes/loanRoutes.js";
 
-dotenv.config();
+import path from "path";
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+dotenv.config({ path: path.join(__dirname, "../.env") });console.log("Checking MONGO_URI:", process.env.MONGO_URI ? "Found" : "NOT FOUND");
+
 
 const app = express();
 const corsOptions = {
